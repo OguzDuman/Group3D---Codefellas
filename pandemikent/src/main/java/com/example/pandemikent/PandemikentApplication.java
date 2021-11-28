b04f422bf36ac2c4b63627a7f03cf4f1cee333d4
AmmaarIftikhar <hakim.ammaar@gmail.com>
2021-11-28 15:51:18 +0300

    Spring boot initializer

---
+++ pandemikent/src/main/java/com/example/pandemikent/PandemikentApplication.java
@@ -0,0 +1,13 @@
+package com.example.pandemikent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandemikentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PandemikentApplication.class, args);
+	}
+
+}
---
+++ pandemikent/src/test/java/com/example/pandemikent/PandemikentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pandemikent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PandemikentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
