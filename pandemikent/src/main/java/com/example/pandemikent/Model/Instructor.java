e6589987dd6dea44ff7822a6d2d0405905b05b99
AmmaarIftikhar <hakim.ammaar@gmail.com>
2021-12-04 21:03:02 +0300

    -v3 update

--- pandemikent/src/main/java/com/example/pandemikent/Model/Instructor.java
+++ pandemikent/src/main/java/com/example/pandemikent/Model/Instructor.java
@@ -1,14 +1,52 @@
-// package com.example.pandemikent.Model;
+package com.example.pandemikent.Model;
 
-// import java.util.ArrayList;
+import java.util.List;
+import java.util.ArrayList;
 
-// import javax.persistence.DiscriminatorValue;
-// import javax.persistence.Entity;
+import javax.persistence.CollectionTable;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
 
-// @Entity
-// @DiscriminatorValue("Instructor")
-// public class Instructor extends UserProfile{
-//     // The identifies in the super class will automatically be used 
-//     // as an identifier here
-//     //private ArrayList<Section> section =  new ArrayList<>();
-// }
+@Entity
+@DiscriminatorValue("Instructor")
+public class Instructor extends UserProfile{
+    @ElementCollection
+    @CollectionTable(name ="history")
+    private List<String> history = new ArrayList<>();
+
+    @ElementCollection
+    @CollectionTable(name ="sectionsInstructor")
+    private List<String> sections = new ArrayList<>();
+
+    public Instructor(String username, int id, String email) {
+        super(username, id, email);
+    }
+
+    public Instructor() {
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "Instructor [history=" + history + ", sections=" + sections + "]";
+    }
+
+    public List<String> getHistory() {
+        return history;
+    }
+
+    public void setHistory(List<String> history) {
+        this.history = history;
+    }
+
+    public List<String> getSections() {
+        return sections;
+    }
+
+    public void setSections(List<String> sections) {
+        this.sections = sections;
+    }
+
+    
+}
+
--- pandemikent/src/main/java/com/example/pandemikent/Model/Student.java
+++ pandemikent/src/main/java/com/example/pandemikent/Model/Student.java
@@ -4,26 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.CollectionTable;
-import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="student")
 @DiscriminatorValue("Student")
 public class Student extends UserProfile {
-    // The identifies in the super class will automatically be used 
-    // as an identifier here
-    // @Id
-    // @Column(name="username")
-    // private String username;
-    // @Column(name="id")
-    // private int id;
-    // @Column(name="email")
-    // private String email;
     
     @ElementCollection
     @CollectionTable(name ="classList")
@@ -33,35 +22,9 @@ public class Student extends UserProfile {
     @CollectionTable(name ="history")
     private List<String> history = new ArrayList<>();
 
-    // public Student(String username, int id, String email) {
-    //     this.username = username;
-    //     this.id = id;
-    //     this.email = email;
-    // }
-
-    // public String getUsername() {
-    //     return username;
-    // }
-
-    // public void setUsername(String username) {
-    //     this.username = username;
-    // }
-
-    // public int getId() {
-    //     return id;
-    // }
-
-    // public void setId(int id) {
-    //     this.id = id;
-    // }
-
-    // public String getEmail() {
-    //     return email;
-    // }
-
-    // public void setEmail(String email) {
-    //     this.email = email;
-    // }
+    @ElementCollection
+    @CollectionTable(name ="closeContacts")
+    private List<String> closeContacts = new ArrayList<>();
 
     public List<String> getClasses() {
         return classes;
