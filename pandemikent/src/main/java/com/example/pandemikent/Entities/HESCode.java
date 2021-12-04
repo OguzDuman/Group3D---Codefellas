c987b234f323c74abcf1fc222d778bae22263329
Esra Genç <esragenc522@gmail.com>
2021-12-04 18:00:15 +0300

    5 Entities and 2 enums implemented
    5 entities: class, section, hesCode, pcr, vaccine
    2 enums: timeSlot, VaccineType

---
+++ pandemikent/src/main/java/com/example/pandemikent/Entities/HESCode.java
@@ -0,0 +1,62 @@
+package com.example.pandemikent.Entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "HES_CODE")
+public class HESCode {
+
+	// Properties
+	@Id
+	@Column(name = "hes_code_id")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seqhes")
+	@SequenceGenerator(name = "seqhes", sequenceName = "HES_SEQ", allocationSize = 1)
+	private Long hesCodeId;
+	
+	@Column(name = "code", nullable = false)
+	private String code;
+	
+	@Column(name = "patient_id", nullable = false)
+	private String patientId;
+
+	// Constructors
+	public HESCode() {
+	}
+
+	// Getters and Setters
+	public HESCode(Long hesCodeId, String code, String patientId) {
+		this.hesCodeId = hesCodeId;
+		this.code = code;
+		this.patientId = patientId;
+	}
+
+	public Long getHesCodeId() {
+		return hesCodeId;
+	}
+
+	public void setHesCodeId(Long hesCodeId) {
+		this.hesCodeId = hesCodeId;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getPatientId() {
+		return patientId;
+	}
+
+	public void setPatientId(String patientId) {
+		this.patientId = patientId;
+	}
+}
---
+++ pandemikent/src/main/java/com/example/pandemikent/Entities/VaccineType.java
@@ -0,0 +1,5 @@
+package com.example.pandemikent.Entities;
+
+public enum VaccineType {
+	PFIZER_BIONTECH, SINOVAC_CORONAVAC
+}
