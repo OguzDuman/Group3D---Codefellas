53dffd0156401bebe82dd348226e4329f0b17b5b
AmmaarIftikhar <hakim.ammaar@gmail.com>
2021-12-14 18:46:18 +0300

    merging

---
+++ pandemikent/src/main/java/com/example/pandemikent/Controller/CovidHistoryController.java
@@ -0,0 +1,17 @@
+// package com.example.pandemikent.Controller;
+
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.ResponseBody;
+
+// public class CovidHistoryController {
+//     @Autowired
+//     private CovidHistoryService covidInfo;
+
+//     @GetMapping("/vaccineHistory")
+//     public @ResponseBody String displayCovidHistoryPage(@RequestParam String name) {
+//         // I don't understand why the method in the service class is returning a boolean. It should return string.
+//         return covidInfo.getCovidHistory(name);
+//     }
+// }
